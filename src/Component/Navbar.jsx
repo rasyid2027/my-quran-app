e5e9296022abf7dc8683d5b58ab81a8b30d93755
rasyid2027 <halimurrasyid56@gmail.com>
2021-02-04 09:08:30 +0700

    make navbar fixed

--- src/Component/Navbar.jsx
+++ src/Component/Navbar.jsx
@@ -4,15 +4,19 @@ import { NavLink, withRouter } from 'react-router-dom'
 class Navbar extends Component {
     render() {
         return (
-            <nav className="nav-wrapper teal">
-                <div className="container">
-                    <a  className="brand-logo">My Qur'an</a>
-                    <ul className="right">
-                        <li><NavLink exact to="/" activeStyle={{background: '#00867b'}}>Home</NavLink></li>
-                        <li><NavLink to="/surah" activeStyle={{background: '#00867b'}}>Surah</NavLink></li>
-                    </ul>
-                </div>
-            </nav>
+            <div className="navbar-fixed">
+                <nav>
+                    <div className="nav-wrapper teal">
+                        <div className="container">
+                            <a  className="brand-logo">My Qur'an</a>
+                            <ul className="right">
+                                <li><NavLink exact to="/" activeStyle={{background: '#00867b'}}>Home</NavLink></li>
+                                <li><NavLink to="/surah" activeStyle={{background: '#00867b'}}>Surah</NavLink></li>
+                            </ul>
+                        </div>
+                    </div>
+                </nav>
+            </div>
         )
     }
 }
