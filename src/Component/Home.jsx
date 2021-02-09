e1e99e4e9f85df12b382faaa05660c294d019310
rasyid2027 <halimurrasyid56@gmail.com>
2021-02-09 10:28:04 +0700

    change content of home page

--- src/Component/Home.jsx
+++ src/Component/Home.jsx
@@ -1,9 +1,12 @@
-
+import Alquran from '../alquran.png'
 
 function Home() {
     return (
-        <div className="container">
-            <h5 className='center'>welcome to 'My Quran App'</h5>
+        <div className="home container">
+            {/* <h5 className='center'>welcome to 'My Quran App'</h5> */}
+            <div className="logo">
+                <img src={ Alquran } alt="alquran logo" className="alquran"/>
+            </div>
         </div>
     )
 }
